fa83da1e63d270ea9e0ed291edc182a6400c7158
Yashs1623 <yashs230602@gmail.com>
2022-05-31 19:23:06 +0530

    Voice bug fixing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,42 +3,45 @@ import {
     View,
     TouchableOpacity,
     StyleSheet,
-    ActivityIndicator
+    ActivityIndicator,
 } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import Voice from '@react-native-community/voice';
-
+import Voice from '@react-native-voice/voice';
 
 
 function Footer() {
 
+    Voice.onSpeechStart = onSpeechStartHandler;
+    Voice.onSpeechEnd = onSpeechEndHandler;
+    Voice.onSpeechResults = onSpeechResultsHandler;
+    
     const [isloading, setIsloading] = useState(false);
 
-    useEffect(() => {
-        Voice.onSpeechStart = onSpeechStartHandler;
-        Voice.onSpeechEnd = onSpeechEndHandler;
-        Voice.onSpeechResults = onSpeechResultsHandler;
-
-        return () => {
-            Voice.destroy().then(Voice.removeAllListeners)
-        }
+    useEffect(async() => {
+        
+        console.log(await Voice.getSpeechRecognitionServices());
+        // return () => {
+        //     Voice.destroy().then(Voice.removeAllListeners);
+        // }
     }, [])
 
 
     const onSpeechStartHandler = (e) => {
+        console.log("start");
         console.log("onSpeechStart", e);
     }
 
     const onSpeechEndHandler = (e) => {
-        setIsloading(false);
+        console.log("end");
+        // setIsloading(false);
         console.log("onSpeechEnd", e);
     }
 
     const onSpeechResultsHandler = (e) => {
-        console.log('yash');
-        const value = e.value[0];
-        console.log("onSpeechResults", value);
+        console.log('result');
+        let text = e.value[0];
+        console.log("onSpeechResults", text);
         // if (value == "go to quiz page") {
         //     setIsloading(false);
         //     Voice.destroy().then(Voice.removeAllListeners)
@@ -49,9 +52,11 @@ function Footer() {
 
     const startRecording = async () => {
         console.log('Yash Shah');
-        setIsloading(true)
+        setIsloading(true);
         try {
-            await Voice.start('en-Us')
+            await Voice.start('en-Us');
+            console.log(await Voice.isRecognizing());
+            // console.log(res);
         } catch (error) {
             console.log("Error: ", error)
         }
