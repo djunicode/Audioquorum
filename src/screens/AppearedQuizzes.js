05682a5400a2bb303e4e6796d54d8501bc1c629d
Tushar <tusharrahuljain@gmail.com>
2022-06-21 15:40:43 +0530

    Appeared quizzes displayed

--- src/screens/AppearedQuizzes.js
+++ src/screens/AppearedQuizzes.js
@@ -1,74 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   View,
   Text,
   StyleSheet,
   FlatList,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert,
 } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Footer from '../components/Footer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-const DATA = [
-  {
-    id: '1',
-    time: '1.08 pm',
-    title: 'Grammar Quiz',
-    subject: 'ENGLISH',
-    due: 'due Apr 15',
-  },
-  {
-    id: '2',
-    time: '1.08 pm',
-    title: 'Grammar Quiz',
-    subject: 'ENGLISH',
-    due: 'due Apr 15',
-  },
-  {
-    id: '3',
-    time: '1.08 pm',
-    title: 'Grammar Quiz',
-    subject: 'ENGLISH',
-    due: 'due Apr 15',
-  },
-  {
-    id: '4',
-    time: '1.08 pm',
-    title: 'Grammar Quiz',
-    subject: 'ENGLISH',
-    due: 'due Apr 15',
-  },
-];
 
-function AppearedQuizzes({navigation}) {
+function AppearedQuizzes({ navigation }) {
 
-  const renderItem = ({ item }) => {
+
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+
+  const getAppearedQuizzes = async () => {
+    var token = await AsyncStorage.getItem('token');
+    // console.log(token + " getScheduledQuizzesToken");
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    try {
+      const response = await fetch("https://audioquorum-api.herokuapp.com/api/test/view/attempted", requestOptions);
+      const json = await response.json();
+      console.log(json.attemptedTests);
+      setData(json.attemptedTests);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getAppearedQuizzes();
+  }, [])
+
+  const renderItem = ({ item, index }) => {
+    // console.log(index);
     return (
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('QuizPage',
-            {
-              title: item.title,
-              subject: item.subject,
-            })
-        }}>
         <View style={styles.scheduleQuizContainer}>
           <View style={{ flexDirection: 'row' }}>
             <View style={{ justifyContent: 'center' }}>
-              <Text style={styles.textStyle}>{item.time}</Text>
+              <Text style={styles.textStyle}>{item.testId.time}</Text>
+              <Text style={styles.textStyle}>{item.testId.duration} sec</Text>
             </View>
 
             <View style={{ marginLeft: wp('3%') }}>
-              <Text style={{ ...styles.textStyle, fontFamily: 'Poppins-SemiBold' }}>{item.title}</Text>
-              <Text style={styles.textStyle}>{item.due}</Text>
-              <Text style={styles.textStyle}>Subject: {item.subject}</Text>
+              <Text style={{ ...styles.textStyle, fontFamily: 'Poppins-SemiBold' }}>{item.testId.name}</Text>
+              <Text style={styles.textStyle}>Due: {item.testId.date}</Text>
+              <Text style={styles.textStyle}>Subject: {item.testId.subject}</Text>
             </View>
           </View>
           <View style={{ flexDirection: 'row' }}>
-            <View style={{ justifyContent: 'center', marginRight:wp('4%') }}>
-                <Text style={styles.textStyle}>24/25</Text>
+            <View style={{ justifyContent: 'center' }}>
+              <View style={styles.buttonStyle}>
+                <Text style={styles.textStyle}>{item.marksObtained}/{item.testId.totalMarks}</Text>
+              </View>
             </View>
             <View>
               <MaterialCommunityIcons
@@ -79,17 +82,22 @@ function AppearedQuizzes({navigation}) {
             </View>
           </View>
         </View>
-      </TouchableOpacity >
     );
   }
 
+
+
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={DATA}
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (<FlatList
+        data={data}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
+        keyExtractor={item => item._id}
+      />)}
+
       <Footer />
     </View>
   )
@@ -99,7 +107,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent:'space-between'
+    justifyContent: 'space-between'
   },
   scheduleQuizContainer: {
     flexDirection: 'row',
@@ -115,8 +123,17 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: 'black',
     fontFamily: 'Poppins',
+    marginLeft:2,
   },
- 
+  buttonStyle: {
+    height: hp('3%'),
+    width: wp('15%'),
+    borderRadius: 5,
+    alignItems: 'center',
+    backgroundColor: '#FFF8F1',
+    elevation: 2,
+    marginRight: wp('3%'),
+  },
 });
 
-export default AppearedQuizzes
+export default AppearedQuizzes;
